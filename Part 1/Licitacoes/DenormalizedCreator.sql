94cd9c72b3d079b0e38f525e69c94c1c96ca08be
lorenzofmann <lorenzofman@gmail.com>
2019-06-18 19:01:52 -0300

    Update part 1 closing it

--- Part 1/Licitacoes/DenormalizedCreator.sql
+++ Part 1/Licitacoes/DenormalizedCreator.sql
@@ -5,19 +5,21 @@ use biddings;
 drop table if exists bidding;
 create table bidding
 (
-	biddingId int,
-    processId int,
-    objectName varchar(200),
-    bidType int,
-    bidState int,
-    superiorAgencyCode int,
-    superiorAgencyName varchar(200),
-    codeUG int,
-    nameUG varchar(200),
-    city varchar(200),
-    publicationDate date,
-    openingDate date,
-    value float,
-    primary key(biddingId)
+	biddingId 			int,
+    processId 			varchar(17), -- max: 17
+    objectName 			varchar(520), -- max: 517
+    bidType 			int,
+    bidState 			int,
+    superiorAgencyCode 	int,
+    superiorAgencyName 	varchar(45), -- max: 45 (Cap)
+    agencyCode			int,
+    agencyName			varchar(45), -- max: 45 (Cap)
+    managementUnitCode 	int,
+    managementUnitName 	varchar(45), -- max: 45 (Cap)
+    city 				varchar(30), -- max: 28
+    publicationDate 	date,
+    openingDate 		date null,
+    value 				float,
+    primary key(biddingId, bidType, managementUnitCode)
 ) engine = InnoDB default charset=latin1;
 
