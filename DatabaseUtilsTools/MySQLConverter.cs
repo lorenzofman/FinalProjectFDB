c0e403920c332321c77f1ff9f7d93fa6d898d50b
lorenzofmann <lorenzofman@gmail.com>
2019-06-19 08:32:21 -0300

    Add MySqlConverter class

---
+++ DatabaseUtilsTools/MySQLConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseUtilsTools
+{
+    // Hardcoded for licitacoes.csv
+    public class SQLConverter : IConsoleRunnable
+    {
+        public string GetCode()
+        {
+            return "sqlize";
+        }
+
+        public void Run(Queue<string> parameters)
+        {
+
+        }
+    }
+}
